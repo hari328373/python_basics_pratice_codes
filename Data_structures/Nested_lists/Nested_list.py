e74b1f71e98684d1e503ebf933c5e0539a13103d
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-05 23:28:26 +0530

    list inside another list

---
+++ Data_structures/Nested_lists/Nested_list.py
@@ -0,0 +1,9 @@
+#take one list inside another list
+a=[[10,20,30],[40,50,60]]
+print(a[0])
+print(a[0][1]) #access the list in inside list
+
+p=[[10,20],[30,40]],[[50,60],[70,80]]
+#         0                    1
+print(p[1])
+print(p[1][1])
