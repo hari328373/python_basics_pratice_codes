3a82272c4c7ccf9af6e6bc178830a5a4b3997738
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-05 23:22:15 +0530

    comparing the lists

---
+++ Data_structures/Mathematical_operations/comparing_lists.py
@@ -0,0 +1,16 @@
+#use comparison operators for List objects
+#equality
+x=["Dog","Cat","Rat"]
+y=["Dog","Cat","Rat"]
+z=["DOG","CAT","RAT"]
+print(x==y)
+print(x==z)
+print(x != z)
+
+#relational operator
+x=[50,20,30]
+y=[40,50,60,100,200]
+print(x>y)
+print(x>=y)
+print(x<y)
+print(x<=y)
