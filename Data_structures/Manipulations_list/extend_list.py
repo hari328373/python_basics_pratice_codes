c46b1a45fa29d2a4e4fb005d3b4e51c7f2df4798
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-05 23:13:32 +0530

    adding the iterable objects into list

---
+++ Data_structures/Manipulations_list/extend_list.py
@@ -0,0 +1,8 @@
+#extend--> To add all items of one list to another list
+
+l1=["Apple","Book","Car"]
+l2=(1,2,3,4,5) # must be iterable like tuple , list,set
+l1.extend(l2) #all items present in l2 will be added to l1
+print(l1)
+
+
