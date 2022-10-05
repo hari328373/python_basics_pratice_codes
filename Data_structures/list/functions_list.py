820c99b75a71c37ef5d4af62d817749de6372cda
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-05 23:03:42 +0530

    functions:count,clear,split

---
+++ Data_structures/list/functions_list.py
@@ -0,0 +1,20 @@
+# Count -->It returns the number of occurrences of specified item in the list
+n=[1,2,2,2,2,3,3]
+print(n.count(1))
+print(n.count(2))
+print(n.count(3))
+print(n.count(4))
+
+#clear --> clear() function to remove all elements of List.
+p=[10,20,30,40]
+print(p)
+p.clear()
+print(p)
+
+#split
+s="Learning Python is very very easy !!!"
+l=s.split() #default space is used to split
+print(l)
+print(type(l))
+
+
