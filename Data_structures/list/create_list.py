f21b5cc8a6b971dff3484c00ed09d931dee42a1e
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-05 23:03:16 +0530

    creating the empty list

---
+++ Data_structures/list/create_list.py
@@ -0,0 +1,14 @@
+# we create empty list as follows
+#by using list()
+l=list()
+print(type(l))
+print(l)
+
+#by using square braces []
+a=[]
+print(type(a))
+print(a)
+
+#list allows mutiple datatypes
+v=[80,True,5.2,"abc"]
+print(v)
