f7ee5d06661e1ca1d0d800544015638208704a29
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-05 23:16:17 +0530

    remove the item in list

---
+++ Data_structures/Manipulations_list/remove_list.py
@@ -0,0 +1,8 @@
+#remove-->to remove specified item from the list
+n=[10,20,10,30]
+n.remove(10)
+print(n)
+
+l=[1,2,3,4,5,1]
+l.remove(1) #item present multiple times then only first occurrence will be removed
+print(l)
