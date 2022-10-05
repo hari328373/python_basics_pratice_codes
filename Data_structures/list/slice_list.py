57fde92dbe529057db93ae6f911b6436d6859026
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-05 23:04:03 +0530

    slicing of list

---
+++ Data_structures/list/slice_list.py
@@ -0,0 +1,8 @@
+#slicing the elements
+#[start:stop:step]
+
+l=[10,50,"abc",5.23,8.5,102,98]
+print(l[0:4])
+print(l[:6:2]) #print elements with step of 2
+print(l[::-1]) #reverse the elements
+print(l[::]) #print all elements
