c9b1665f8eaa205092f9a8213a980ed74eddc09d
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-06 14:09:24 +0530

    Traversing the tuple

---
+++ data_structure_tuple/tuple/traversing_tuple.py
@@ -0,0 +1,13 @@
+#The sequential access of each element in the tuple
+
+#by using while loop
+t=10,20,30,40,50,60,70
+i=0
+while i <len(t):
+    print(t[i])
+    i=i+1
+
+#by using for loop
+p=(1,2,3,4,5)
+for i in p:
+    print(i)
