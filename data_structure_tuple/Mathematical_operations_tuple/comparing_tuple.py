115ed19f09b21acfc053c6c5cad1ad68fab0d138
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-06 14:33:28 +0530

    comparing the elements in tuples

---
+++ data_structure_tuple/Mathematical_operations_tuple/comparing_tuple.py
@@ -0,0 +1,16 @@
+#use comparison operators for List objects
+#equality
+x=("abc","fruit","annie")
+y=("abc","fruit","annie")
+z=("ABC","FRUIT","ANNIE")
+print(x==y)
+print(x==z)
+print(x != z)
+
+#relational operator
+x=(20,50,90)
+y=(40,80,100,500,200)
+print(x>y)
+print(x>=y)
+print(x<y)
+print(x<=y)
