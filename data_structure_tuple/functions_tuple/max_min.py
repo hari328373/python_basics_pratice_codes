197b1cbfafb9cf421559bb2c9c58fc0c0e1f1c9d
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-06 16:20:18 +0530

    maximum and minimum functions

---
+++ data_structure_tuple/functions_tuple/max_min.py
@@ -0,0 +1,9 @@
+#return min and max values according to default natural sorting order
+#max function
+t=(40,10,30,20,50,60)
+print(max(t))
+
+# min function
+t=("a","z","b","c","j","d","e")
+print(min(t))
+print(max(t))
