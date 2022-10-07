05493fe44fa4af6b284a4b76d21ba0a00045a30f
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-07 23:50:47 +0530

    returns common elements in both x and y

---
+++ data_structure_set/set_mathematical_operations/intersection.py
@@ -0,0 +1,7 @@
+# intersection( )-->x.intersection(y) or x & y
+# Returns common elements present in both x and y
+x = {10, 20, 30, 40}
+y = {30, 40, 50, 60}
+print(x.intersection(y))
+print(x & y)
+print(y & x)
