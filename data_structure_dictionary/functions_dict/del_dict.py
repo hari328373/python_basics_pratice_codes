6dfe5a68ea6a8ecfc14f70b1493ec931e74847d9
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-07 20:32:02 +0530

    delete the values in dictionary and delete dictionary

---
+++ data_structure_dictionary/functions_dict/del_dict.py
@@ -0,0 +1,13 @@
+#del d[key]-->It deletes value associated with the specified key
+d={100:"durga",200:"ravi",300:"shiva"}
+print(d)
+del d[100]
+print(d)
+
+#del d-->To delete total dictionary after we cannot access d
+d={100:"Nani",200:"Charan",300:"shiva"}
+print(d)
+del d
+print(d)
+
+
