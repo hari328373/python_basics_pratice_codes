6a5e6007fe2ed1124efa6212a484adb7a95ae951
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-07 23:34:00 +0530

    it doesn't return error when specified element not present in set

---
+++ data_structure_set/set_functions/discard.py
@@ -0,0 +1,8 @@
+# discard(x)-->It removes the specified element from the set.
+# If the specified element not present in the set then we won't get any error
+
+s = {10, 20, 30}
+s.discard(10)
+print(s)
+s.discard(50)
+print(s)
