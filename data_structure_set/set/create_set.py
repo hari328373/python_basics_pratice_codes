d9329da3bce17f19dc4a9b1941c9c10f5caa07ab
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-07 23:08:36 +0530

    create empty set

---
+++ data_structure_set/set/create_set.py
@@ -0,0 +1,13 @@
+#create empty set
+#by using set ()
+s=set()
+print(type(s))
+print(s)
+
+#by using set(any sequence)
+s1=set([10,20,30]) # here list is convert into set
+print(s1)
+print(type(s1))
+
+
+
