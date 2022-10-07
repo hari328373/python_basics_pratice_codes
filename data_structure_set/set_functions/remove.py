13673dabf239fd25a6a956a7c091da2a3376153a
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-07 23:35:12 +0530

    it return error when specified element not present in set

---
+++ data_structure_set/set_functions/remove.py
@@ -0,0 +1,8 @@
+# remove(x)-->It removes specified element from the set.
+# If the specified element not present in the Set then we will get KeyError
+
+s = {40, 10, 30, 20}
+s.remove(30)
+print(s)
+s.remove(50)
+print(s)
