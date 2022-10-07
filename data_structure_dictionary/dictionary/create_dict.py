534d3555b30ec2ad78e704d1a6af56fd1a94e618
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-07 17:21:13 +0530

    creating the empty dictionary

---
+++ data_structure_dictionary/dictionary/create_dict.py
@@ -0,0 +1,10 @@
+#creating the empty dictionary
+#dictionary represented in {} braces
+d=dict()
+print(type(d))
+print(d)
+
+# represent the elements as key-value pairs in dictionary
+d={101:"apple",102:"banana",103:"manago"}  # here 101,102,103 are keys and apple,banana,manago are values
+print(type(d))
+print(d)
