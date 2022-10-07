263a6cfa3b2454d9a499f90a8505cb941426e138
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-07 23:55:09 +0530

    sample program

---
+++ data_structure_set/set_sample_program/sample_program.py
@@ -0,0 +1,6 @@
+# program to print vowels present in the given word
+w = input("Enter word to search for vowels: ")
+s = set(w)
+v = {'a', 'e', 'i', 'o', 'u'}
+d = s.intersection(v)
+print("The different vowel present in", w, "are", d)
