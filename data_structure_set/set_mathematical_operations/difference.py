1120228523ef21e1cd23757da1d6a0b75f42fa3c
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-07 23:50:19 +0530

    returns the elements present in x but not in y

---
+++ data_structure_set/set_mathematical_operations/difference.py
@@ -0,0 +1,7 @@
+# difference( )-->x.difference(y) or x - y
+# returns the elements present in x but not in y
+x = {10, 20, 30, 40}
+y = {30, 40, 50, 60}
+print(x.difference(y))
+print(x-y)
+print(y-x)
