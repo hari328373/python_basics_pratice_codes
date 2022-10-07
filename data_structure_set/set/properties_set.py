5541ee7876544864ccf781bcf0b6e4171759dcda
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-07 23:09:30 +0530

    properties of set data structure

---
+++ data_structure_set/set/properties_set.py
@@ -0,0 +1,13 @@
+# set doesn't allow duplicates
+s={1,1,1,2,0,3,5,4,8,7,7,5,9}
+print(s)
+
+# set won't support index or slicing
+s={0, 1, 2, 3, 4, 5, 7, 8, 9}
+print(s[0])
+print(s[:])
+
+#multiple data types are allowed
+s={10,2.5,"abc",True}
+print(s)
+
