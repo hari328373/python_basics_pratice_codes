5fc87d1f3127793205b5615cb8be33ea20e6c88b
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-07 23:51:20 +0530

    return all elements present in both sets

---
+++ data_structure_set/set_mathematical_operations/union.py
@@ -0,0 +1,7 @@
+# union( )-->x.union(y) or x | y
+# x.union(y) ==>to return all elements present in both sets
+x = {10, 20, 30, 40}
+y = {30, 40, 50, 60}
+print(x.union(y))
+print(x | y)
+print(y | x)
